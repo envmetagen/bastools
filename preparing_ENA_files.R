2572b1d5d757e31ec33cec59321bf6f9cd1925d6
bastianegeter@yahoo.ie <bastianegeter@yahoo.ie>
2019-11-25 22:00:47 +0000

    ENA upload

--- preparing_ENA_files.R
+++ preparing_ENA_files.R
@@ -1,7 +1,7 @@
 #uploading ENA data via Aspera
 
 #CONFIG
-bastoolsDir<-"/home/bastian.egeter/git_bastools/bastools/"
+bastoolsDir<-"/Users/basti/Documents/WORK/CIBIO/STATS_AND_CODE/git-bastools/bastools/"
 
 #filturb datasheet
 sheeturl<-"https://docs.google.com/spreadsheets/d/1FUSaeVaYzms2EOGUoCAB4jaRKzguD3AKTsC8lYwaKP4/edit?ts=5dae01be#gid=1531090624"
@@ -17,10 +17,15 @@ email="basegeter@gmail.com"
 
 #directory with files for upload
 fileDir<-"/mnt/Disk2/MISEQ_RUNS/2018_07_FILTURB_IRANVERTS/FASTQ_GENERATION_3_18/"
-##note that the ENA upload sheets will be saved here
+
+#outdir ENA upload sheets will be saved here
+outdir<-"/Users/basti/Documents/WORK/CIBIO/AA_PROJECTS/FILTURB/ENA/"
 
 #final sample sheet filename
-sample_sheet<-"FILTURB_NORTH_ENA_SAMPLE_SHEET.txt"
+sample_sheet_name<-"FILTURB_NORTH_ENA_SAMPLE_SHEET.txt"
+
+#final library sheet filename
+library_sheet_name<-"FILTURB_NORTH_ENA_LIBRARY_SHEET.txt"
 
 #name to give list of files
 filelist<-"FILTURB_NORTH_ENA_FILES.txt"
@@ -30,7 +35,7 @@ filelist<-"FILTURB_NORTH_ENA_FILES.txt"
 setwd(bastoolsDir)
 googlesheets4::sheets_auth(email = email)
 source("googlesheet.foos.R")
-setwd(fileDir)
+setwd(outdir)
 
 ########################################SAMPLE DATA
 #first read ENA_sample_data sheet
@@ -49,8 +54,8 @@ master_xtabs(sub_sample_sheet,columns=c("sample_type",names(sample.subsetlist)))
 
 #write - Needed here?
 write.table(data.frame(V1=c("#checklist_accession","#unique_name_prefix"),V2=c("ERC000011","")),
-            file = sample_sheet,quote = F,row.names = F,sep = "\t",col.names = F)
-write.table(sub_sample_sheet,file = sample_sheet,quote = F,row.names = F,sep = "\t",append = T)
+            file = sample_sheet_name,quote = F,row.names = F,sep = "\t",col.names = F)
+write.table(sub_sample_sheet,file = sample_sheet_name,quote = F,row.names = F,sep = "\t",append = T)
 
 
 ########################################LIBRARY DATA
@@ -66,13 +71,15 @@ library_sheet<-google.read.master.url(sheeturl,ws = "ENA_library_data")
 #subset by sample sheet
 library_sheet<-library_sheet[library_sheet$library_name %in% sub_master_sheet$library_name,]
 
+#write - Needed here?
+write.table(x = library_sheet,file = library_sheet_name,quote = F,row.names = F,sep = "\t",col.names = T)
+
 ########################################UPLOAD SEQUENCES
 #get filenames and check they exist
 files<-c(library_sheet$forward_file_name,library_sheet$reverse_file_name)
-message(paste(sum(files %in% list.files()), "of", length(files), "files found"))
+message(paste(sum(files %in% list.files()), "of", length(files), "files found, of which",files[duplicated(files)], "are duplicates"))
+
 write.table(files,file = filelist,quote = F,row.names = F,sep = "\t",col.names = F)
 
-#cp files to temp folder
 
-#run Aspera
             
