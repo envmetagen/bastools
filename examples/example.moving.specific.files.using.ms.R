4e0d59ce2f256bdda2d002e2b58cffb50b283353
bastianegeter@yahoo.ie <bastianegeter@yahoo.ie>
2020-07-28 12:58:19 +0100

    update

--- examples/example.moving.specific.files.using.ms.R
+++ examples/example.moving.specific.files.using.ms.R
@@ -1,5 +1,8 @@
-source("/home/bastian.egeter/git_bastools/bastools/master_functions.R")
+#source("/home/bastian.egeter/git_bastools/bastools/master_functions.R")
 
+source("/home/tutorial/TOOLS/bastools/master_functions.R")
+
+#set to original file directory
 setwd("/mnt/Disk2/MISEQ_RUNS/2018_02_MICROSATS-SOILPHOS-FILTURB-NZFROG-IRANVERTS-ICVERTS-GUELTA/original_miseq_files/")
 
 a<-google.overlord("https://docs.google.com/spreadsheets/d/1E0LjEqJx4VJQ2pZEPf5EKrhHdkGeo99ZviKjaXxZCPs/edit#gid=0",
@@ -8,7 +11,10 @@ a<-google.overlord("https://docs.google.com/spreadsheets/d/1E0LjEqJx4VJQ2pZEPf5E
 files<-unlist(stringr::str_split(a$sequencing_filenames," "))
 
 for(i in 1:length(files)){
-  file.copy(from = files[i],to = "./temp.copy/")
+  file.copy(from = files[i],to = "./temp/") #must exist
 }
 
 write.table(a,"./temp.copy/SOILPHOS_2018_02_PHOD_master.txt",append = F,quote = F,row.names = F,sep = "\t")
+
+file.copy(from = "test.binthresh.out.tsv",to = "./temp/")
+file.remove("./temp/test.binthresh.out.tsv")
