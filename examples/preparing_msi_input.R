f0021bc111672cccbc3106ac6649805341fe4a64
bastianegeter@yahoo.ie <bastianegeter@yahoo.ie>
2020-08-06 00:36:46 +0100

    update

--- examples/preparing_msi_input.R
+++ examples/preparing_msi_input.R
@@ -1,7 +1,7 @@
 #preparing msi
 url="https://docs.google.com/spreadsheets/d/14QeZ0KyEHpqCZ3dsj4a6iojxnuLbasR7Z6y68FK9vn8/edit#gid=1377121809"
-METADATAFILE="/mnt/Disk1/BASTIAN_POST_MBC_MISEQS/MINION/2020_July_NM/master_sheet.txt"
-subsetList=list(experiment_id="2020_July_NM")
+METADATAFILE="/mnt/Disk1/BASTIAN_POST_MBC_MISEQS/MINION/2020_July_NM_B/master_sheet.txt"
+subsetList=list(experiment_id="2020_July_NM_B")
 
 ##############################################################
 
